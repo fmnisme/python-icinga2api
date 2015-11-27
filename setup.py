7676d6b4938db783b028cc877db3d3c350229f6c
fmnisme <fmnisme@gmail.com>
2015-11-27 10:09:32 +0800

    上传到 pypi

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+PACKAGE = "icinga2api"
+NAME = "python-icinga2api"
+DESCRIPTION = "python icinga2 api "
+AUTHOR = "fmnisme"
+AUTHOR_EMAIL = "fmnisme@gmail.com"
+URL = "https://github.com/fmnisme/python-icinga2api"
+VERSION = __import__(PACKAGE).__version__
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="BSD",
+    url=URL,
+    packages=find_packages(),
+    zip_safe=False,
+    long_description=read('README.md'),
+)
